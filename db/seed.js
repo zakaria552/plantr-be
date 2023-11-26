32682795c4874f3c08ec99c847d76e3aa4e150c1
zakaria farah <zakaria.farah55@outlook.com>
2023-11-26 08:45:45 +0000

    seeds the plants to db

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,13 @@
 const db = require("./connection");
 const usersData = require("./data/users");
+const plantsData = require("./data/plants")
 const UsersModel = require("./schema/user");
+const PlantsModel = require("./schema/plants")
 async function seed({ usersData }) {
     return db.then(async (mongoose) => {
         await mongoose.connection.db.dropDatabase()
         await UsersModel.insertMany(usersData)
+        await PlantsModel.insertMany(plantsData)
         return mongoose
     })
 }
